29a0e7eab4e00db0417b0fb8cd42bce9f345e492
abdulghani200 <hackerghani007@gmail.com>
2019-06-03 12:45:16 +0530

    updated about page

--- src/About.js
+++ src/About.js
@@ -9,6 +9,7 @@ class About extends Component{
             <div>
                 <Header />
                 <h3 style={{color: "white", textAlign: "center", marginTop: "40vh"}}>A simple Weather App built using React, Google Maps and OpenWeather. </h3>
+                <p style={{textAlign: "center"}}>Developed by <a href="https://abdulghani.me" style={{color: "#ffee07"}}>Abdul Ghani</a>< br/> Fork this project on <a href="https://github.com/abdulghani200/weatherApp" style={{color: "#212529"}}>Github</a></p>
             </div>
         );
     }
